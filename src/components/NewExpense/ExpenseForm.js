2b6944049070c07de9f382c5f0a2200447a0b95f
curtis <hong.curtis@gmail.com>
2022-04-23 06:41:15 -0700

    clear form upon submission using new state object upon form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-
   // const [enteredTitle, setEnteredTitle] = useState('')
   // const [enteredAmount, setEnteredAmount] = useState('')
   // const [enteredDate, setEnteredDate] = useState('')
 
   const [enteredInput, setEnteredInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: '',
-  })
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
-  const titleChangeHandler = (event)=>{
+  const titleChangeHandler = (event) => {
     // setEnteredTitle(event.target.value)
     // setEnteredInput({
     //   ...enteredInput,
@@ -21,62 +20,82 @@ const ExpenseForm = () => {
     // })
 
     // React schedules state updates, which is why you should explicitly use close to reutrn previous state spread operator
-    // safer to work on the latest state 
-    setEnteredInput((prevState)=>{
-      return {...prevState, enteredTitle: event.target.value}
-    })
+    // safer to work on the latest state
+    setEnteredInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
   };
 
-  const amountChangeHandler = (event)=>{
+  const amountChangeHandler = (event) => {
     // setEnteredAmount(event.target.value)
     // setEnteredInput({
     //   ...enteredInput,
     //   enteredAmount: event.target.value
-    setEnteredInput((prevState)=>{
-      return {...prevState, enteredAmount: event.target.value}
-    })
+    setEnteredInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    });
   };
-  const dateChangeHandler = (event)=>{
+  const dateChangeHandler = (event) => {
     // setEnteredDate(event.target.value)
     // setEnteredInput({
     //   ...enteredInput,
     //   enteredDate: event.target.value
     // })
-    setEnteredInput((prevState)=>{
-      return {...prevState, enteredDate: event.target.value}
-    })
+    setEnteredInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
   };
-  
 
-  const submitHandler = (event) =>{
+  const submitHandler = (event) => {
     event.preventDefault();
 
     const expenseData = {
       title: enteredInput.enteredTitle,
       amount: enteredInput.enteredAmount,
-      date: new Date(enteredInput.enteredDate) 
-    }
-    console.log(expenseData)
+      date: new Date(enteredInput.enteredDate),
+    };
+    console.log(expenseData);
+    setEnteredInput({
+      enteredDate:"",
+      enteredAmount: "",
+      enteredTitle: "" 
+    })
   };
 
   return (
-    <form on onSubmit={submitHandler}>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text"  onChange={titleChangeHandler}/>
+          <input
+            type="text"
+            value={enteredInput.enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min ="0.01" step = "0.01" onChange={amountChangeHandler}/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredInput.enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min ="2019-01-01" max = "2022-12-31" onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredInput.enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-          <button type = "submit">Add Expense</button>
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
