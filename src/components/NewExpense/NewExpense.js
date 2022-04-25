3b6d406a006e4e2b890372ffdf6ae7c80774f718
curtis <hong.curtis@gmail.com>
2022-04-24 18:53:30 -0700

    pass data from child to parent components as a parameter in the child component when calling the parent component prop function

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { v4 as uuidv4 } from 'uuid';
 
 const NewExpense = () => {
+  const saveExpenseDateHandler = (enteredExpenseData) => {
+    const expenseDate = {
+      ...enteredExpenseData,
+      id: uuidv4()
+    };
+    console.log(expenseDate)
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData = {saveExpenseDateHandler} />
     </div>
   );
 };
