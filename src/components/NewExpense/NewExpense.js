7bae24a7c07eb57f6bccd87bd09d17e38ce6b889
curtis <hong.curtis@gmail.com>
2022-04-29 16:31:26 -0700

    refactor if statement to boolean to conditionally render via formStatus

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,10 +4,14 @@ import ExpenseForm from "./ExpenseForm";
 import { v4 as uuidv4 } from "uuid";
 
 const NewExpense = ({ onAddExpense }) => {
-  const [formStatus, SetFormStatus] = useState("off");
+  const [formStatus, SetFormStatus] = useState(false);
 
-  const formStatusHandlerOn =()=>{SetFormStatus("on")};
-  const formStatusHandlerOff =()=>{SetFormStatus("off")};
+  const formStatusHandlerOn = () => {
+    SetFormStatus(true);
+  };
+  const formStatusHandlerOff = () => {
+    SetFormStatus(false);
+  };
 
   const saveExpenseDateHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -16,23 +20,23 @@ const NewExpense = ({ onAddExpense }) => {
     };
     console.log(expenseData);
     onAddExpense(expenseData);
-    formStatusHandlerOff()
+    formStatusHandlerOff();
   };
-
-  if (formStatus === "off") {
-    return (
-      <div className="new-expense">
-        <button className="new-expense button" onClick={formStatusHandlerOn}>
-          Add New Expense
-        </button>
-      </div>
-    );
-  }
-  return (
-    <div className="new-expense">
-      <ExpenseForm cancel={formStatusHandlerOff} onSaveExpenseData={saveExpenseDateHandler} />
-    </div>
-  );
+  return(
+  <div className="new-expense">
+    {!formStatus && 
+      <button className="new-expense button" onClick={formStatusHandlerOn}>
+        Add New Expense
+      </button>
+    }
+    {formStatus && (
+      <ExpenseForm
+        cancel={formStatusHandlerOff}
+        onSaveExpenseData={saveExpenseDateHandler}
+      />
+    )}
+  </div>
+  )
 };
 
 export default NewExpense;
