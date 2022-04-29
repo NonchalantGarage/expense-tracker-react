c1f4ab3ccda28955e353fc7172f0daa62046eb71
curtis <hong.curtis@gmail.com>
2022-04-29 15:31:37 -0700

    move conditional rendering for ExpensesList and changed conditional statement to render jsx for no expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList"
+
 
 function Expenses(props) {
   const [pickedYear, newPickedYear] = useState(
@@ -18,23 +19,10 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === pickedYear;
   });
 
-let expensesContent = <p>No expenses found.</p>
-
-if(filteredYearArray.length > 0 ){
-  expensesContent = filteredYearArray.map((expense) => (
-    <ExpenseItem
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-      key={expense.id}
-    />
-  ))
-}
-
   return (
     <Card className="expenses">
       <ExpenseFilter pickedYear={pickedYear} onPickYear={pickedYearHandler} />
-     {expensesContent}
+     <ExpensesList items = {filteredYearArray}/>
     </Card>
   );
 }
